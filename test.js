ab2fd99fce6b0eded020ae2f95eb9c12f56e8517
kamranhanif012 <mkamranhanif012@gmail.com>
2025-07-21 16:18:59 +0500

    basic operations

---
+++ test.js
@@ -0,0 +1,23 @@
+function sum(a, b){
+    console.log(a+b) 
+}
+
+function sub(a, b){
+    console.log(a-b)
+}
+
+function multiply(a, b){
+    console.log(a*b)
+}
+
+function div(a, b){
+    console.log(a/b)
+}
+
+sum(4,2)
+sub(5,8)
+div(2,9)
+multiply(43,98)
+
+
+
