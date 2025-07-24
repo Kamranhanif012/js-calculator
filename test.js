5df0936bf624aa7436723876b6404ab3d3b01f7b
Kamranhanif012 <157672869+Kamranhanif012@users.noreply.github.com>
2025-07-24 16:19:45 +0500

    Functional calculator 2 (#4)
    * functioning calculator
    
    * styles
    
    * styless
    
    * calculator done with eventlistener
    
    * eval function changed

--- test.js
+++ test.js
@@ -1,23 +1,44 @@
-function sum(a, b){
-    console.log(a+b) 
+const display= document.getElementById("input") //selects input element
+
+
+
+function addToDisplay(value){
+    display.value += value
 }
 
-function sub(a, b){
-    console.log(a-b)
+//numbers
+const action= document.querySelectorAll("#num-btn") //selects all buttons except = and C
+
+action.forEach( function(btn){
+    btn.addEventListener('click' , number )  //loop to loop over all buttons and assign them an eventlistener or execute a function defined here
+})
+
+
+function number(){
+            const value= event.target.innerHTML; // go to the target button reads it html content and place it in value variable to display it in input
+            addToDisplay(value)
 }
 
-function multiply(a, b){
-    console.log(a*b)
+//evaluation
+
+const evaluation = document.getElementById("eval-btn") // selects
+
+evaluation.addEventListener('click' , evaluate ) //add eventlistener
+
+function evaluate(){
+    if (display.value==0){
+    display.value="can't do :)"  //condition for not solving 0
+}
+    display.value = eval(display.value) // evalution function
+ 
 }
 
-function div(a, b){
-    console.log(a/b)
+
+//clear screen
+const clear= document.getElementById("clr-btn") // selects
+
+clear.addEventListener('click', clearscreen); //adds eventlistener
+
+function clearscreen(){
+    display.value=''  // clears the input
 }
-
-sum(4,2)
-sub(5,8)
-div(2,9)
-multiply(43,98)
-
-
-
